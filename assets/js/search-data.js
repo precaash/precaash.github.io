7ec9ac473d3278ac0b88d11bb36d061edae63c1f
precaash <47886635+precaash@users.noreply.github.com>
2026-02-03 13:57:42 +0000

    Deploying to gh-pages from @ precaash/precaash.github.io@40ff31246998f0f8bba2ebc1b6fbf1e01a9a796f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,28 +16,14 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-cv",
-          title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+        },{id: "nav-curriculum-vitae",
+          title: "curriculum vitae",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/blog/";
-              },
-            },{id: "post-a-post-with-plotly-js",
+        },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
